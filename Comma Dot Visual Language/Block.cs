38efbf4217f9525d578b1fef28e05f6e974d2899
DESKTOP-U1RPPKA\jbilińskipl <jbilinskipl@gmail.com>
2017-04-26 22:10:36 +0200

    Changed shape of input block and added auto move after creating new block.

--- Comma Dot Visual Language/Block.cs
+++ Comma Dot Visual Language/Block.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 
 namespace Comma_Dot_Visual_Language
 {
@@ -14,7 +15,10 @@ namespace Comma_Dot_Visual_Language
         public static int BlocksCounter = 0;
         public Block NextBlockPrimary { get; set; }
         public Block NextBlockOptional { get; set; }
+
         protected Canvas CanvasBlocks;
+        protected Polygon _shape;
+        protected bool _isPressed;
 
         protected Block(Canvas canvas)
         {
--- Comma Dot Visual Language/InputBlock.cs
+++ Comma Dot Visual Language/InputBlock.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -12,22 +13,21 @@ namespace Comma_Dot_Visual_Language
 {
     class InputBlock : Block
     {
-        private Rectangle _shape;
-        private bool _isPressed = false;
-
         public InputBlock(Canvas canvas) : base(canvas)
         {
-            _shape = new Rectangle
+            _shape = new Polygon()
             {
-                Width = 50,
-                Height = 50,
                 Stroke = new SolidColorBrush(Colors.Black),
                 Fill = new SolidColorBrush(Colors.White),
+                Points = {new Point(0,0), new Point(20,30), new Point(100,30), new Point(80,0)}
             };
+
             canvas.Children.Add(_shape);
             canvas.Children[canvas.Children.Count - 1].MouseLeftButtonDown += OnMouseLeftButtonDown;
             canvas.Children[canvas.Children.Count - 1].MouseLeftButtonUp += OnMouseLeftButtonUp;
             canvas.Children[canvas.Children.Count - 1].MouseMove += OnMouseMove;
+
+            _isPressed = true;
         }
 
         private void OnMouseMove(object sender, MouseEventArgs mouseEventArgs)
@@ -35,7 +35,7 @@ namespace Comma_Dot_Visual_Language
             if (!_isPressed)
                 return;
 
-            Rectangle tmpRectangle = (Rectangle)sender;
+            Polygon tmpRectangle = (Polygon)sender;
             var mousePosition = mouseEventArgs.GetPosition(CanvasBlocks);
             double left = mousePosition.X - (tmpRectangle.ActualWidth / 2);
             double top = mousePosition.Y - (tmpRectangle.ActualHeight / 2);
@@ -45,14 +45,14 @@ namespace Comma_Dot_Visual_Language
 
         private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            Rectangle tmpRectangle = (Rectangle)sender;
+            Polygon tmpRectangle = (Polygon)sender;
             tmpRectangle.ReleaseMouseCapture();
             _isPressed = false;
         }
 
         private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
         {
-            Rectangle tmpRectangle = (Rectangle)sender;
+            Polygon tmpRectangle = (Polygon)sender;
             tmpRectangle.CaptureMouse();
             _isPressed = true;
         }
