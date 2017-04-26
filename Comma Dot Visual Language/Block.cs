91aa03d263bddbe44c54de701e211612019c8ba6
DESKTOP-U1RPPKA\jbilińskipl <jbilinskipl@gmail.com>
2017-04-26 22:25:44 +0200

    Code from InputBlock has been moved to block.

--- Comma Dot Visual Language/Block.cs
+++ Comma Dot Visual Language/Block.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Shapes;
 
 namespace Comma_Dot_Visual_Language
@@ -25,6 +26,42 @@ namespace Comma_Dot_Visual_Language
             Id = BlocksCounter++;
             Command = "";
             CanvasBlocks = canvas;
+            _isPressed = true;
+        }
+
+        protected void AddShapeToCanvas()
+        {
+            CanvasBlocks.Children.Add(_shape);
+            CanvasBlocks.Children[CanvasBlocks.Children.Count - 1].MouseLeftButtonDown += OnMouseLeftButtonDown;
+            CanvasBlocks.Children[CanvasBlocks.Children.Count - 1].MouseLeftButtonUp += OnMouseLeftButtonUp;
+            CanvasBlocks.Children[CanvasBlocks.Children.Count - 1].MouseMove += OnMouseMove;
+        }
+
+        protected void OnMouseMove(object sender, MouseEventArgs mouseEventArgs)
+        {
+            if (!_isPressed)
+                return;
+
+            Polygon tmpRectangle = (Polygon)sender;
+            var mousePosition = mouseEventArgs.GetPosition(CanvasBlocks);
+            double left = mousePosition.X - (tmpRectangle.ActualWidth / 2);
+            double top = mousePosition.Y - (tmpRectangle.ActualHeight / 2);
+            Canvas.SetLeft(tmpRectangle, left);
+            Canvas.SetTop(tmpRectangle, top);
+        }
+
+        protected void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            Polygon tmpRectangle = (Polygon)sender;
+            tmpRectangle.ReleaseMouseCapture();
+            _isPressed = false;
+        }
+
+        protected void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            Polygon tmpRectangle = (Polygon)sender;
+            tmpRectangle.CaptureMouse();
+            _isPressed = true;
         }
 
         public abstract string Run();
--- Comma Dot Visual Language/InputBlock.cs
+++ Comma Dot Visual Language/InputBlock.cs
@@ -22,39 +22,7 @@ namespace Comma_Dot_Visual_Language
                 Points = {new Point(0,0), new Point(20,30), new Point(100,30), new Point(80,0)}
             };
 
-            canvas.Children.Add(_shape);
-            canvas.Children[canvas.Children.Count - 1].MouseLeftButtonDown += OnMouseLeftButtonDown;
-            canvas.Children[canvas.Children.Count - 1].MouseLeftButtonUp += OnMouseLeftButtonUp;
-            canvas.Children[canvas.Children.Count - 1].MouseMove += OnMouseMove;
-
-            _isPressed = true;
-        }
-
-        private void OnMouseMove(object sender, MouseEventArgs mouseEventArgs)
-        {
-            if (!_isPressed)
-                return;
-
-            Polygon tmpRectangle = (Polygon)sender;
-            var mousePosition = mouseEventArgs.GetPosition(CanvasBlocks);
-            double left = mousePosition.X - (tmpRectangle.ActualWidth / 2);
-            double top = mousePosition.Y - (tmpRectangle.ActualHeight / 2);
-            Canvas.SetLeft(tmpRectangle, left);
-            Canvas.SetTop(tmpRectangle, top);
-        }
-
-        private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
-        {
-            Polygon tmpRectangle = (Polygon)sender;
-            tmpRectangle.ReleaseMouseCapture();
-            _isPressed = false;
-        }
-
-        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
-        {
-            Polygon tmpRectangle = (Polygon)sender;
-            tmpRectangle.CaptureMouse();
-            _isPressed = true;
+            AddShapeToCanvas();
         }
 
         public override String Run()
